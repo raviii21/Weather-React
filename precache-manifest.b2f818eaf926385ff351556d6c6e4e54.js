6815dbf2f56d1c64af971da8c8c3198353b5f5fa
Ravi Savsani <ravisavsani4@gmail.com>
2023-07-11 20:49:48 +0530

    Updates

---
+++ precache-manifest.b2f818eaf926385ff351556d6c6e4e54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac50052baba116a8e96fbedc6562d575",
+    "url": "/Weather-React/index.html"
+  },
+  {
+    "revision": "1f518f60c983dd708426",
+    "url": "/Weather-React/static/css/main.1e2678b9.chunk.css"
+  },
+  {
+    "revision": "b3c40c76f4e64c2c7d57",
+    "url": "/Weather-React/static/js/2.732d2c9b.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/Weather-React/static/js/2.732d2c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f518f60c983dd708426",
+    "url": "/Weather-React/static/js/main.73ba59df.chunk.js"
+  },
+  {
+    "revision": "b156000db4a851c5b682",
+    "url": "/Weather-React/static/js/runtime-main.a0a2767a.js"
+  }
+]);
